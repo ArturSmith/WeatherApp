5545a992e469fa683447696c0f6384725878a59f
Arthur Smith <arthur9smith7@gmail.com>
2024-02-02 14:44:55 +0100

    Created ApplicationComponent.

---
+++ app/src/main/java/com/smith/weatherapp/di/ApplicationComponent.kt
@@ -0,0 +1,21 @@
+package com.smith.weatherapp.di
+
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+
+@ApplicationScope
+@Component(
+    modules = [
+        DataModule::class,
+        PresentationModule::class
+    ]
+)
+interface ApplicationComponent {
+    @Component.Factory
+    interface Factory {
+        fun create(
+            @BindsInstance context: Context
+        ): ApplicationComponent
+    }
+}
